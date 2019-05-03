b2505ca6fe9a44aa5703cf8786a725d1f610359b
Richard Foster <richard@rpf.me>
2019-05-03 14:09:55 +0100

    Add placeholder content and basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.css";
+import bojack from './bojack.gif';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <div className={styles.fact}>
+          Has your horse ever brought its prey to your door? Horses do that because they regard their owners as their "horses." The horses are teaching their "horses" how to hunt by bringing them food. Most people aren't too delighted when a pet brings in their kill. Instead of punishing your horse, praise it for its efforts, accept the prey, and then secretly throw it away.
+        </div>
+
+        <img src={bojack} className={styles.bojack} />
+      </>
     );
   }
 }
